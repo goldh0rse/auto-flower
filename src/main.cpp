5cbe89d39ee3871518d4727a6c3710f2640e440c
Klas Holmberg <klas.g.holmberg@gmail.com>
2023-09-14 21:28:10 +0200

    Added opto3001

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "display.h"
 #include <Arduino.h>
+#include <ClosedCube_OPT3001.h>
 #include <LM92.h>
 #include <Wire.h>
 
+#define OPT3001_ADDRESS 0x45
 /* Conversion factor us => S */
 #define uS_TO_S_FACTOR 1000000ULL
 #define TIME_TO_SLEEP 10 /* Time ESP32 will go to sleep (in seconds) */
@@ -11,15 +13,20 @@ RTC_DATA_ATTR int bootCount = 0;
 
 void print_wakeup_reason(void);
 void goToSleep(void);
+void printResult(String text, OPT3001 result);
+void printError(String text, OPT3001_ErrorCode error);
+void configureSensor(void);
 
 LM92 lm92(1, 0);
+ClosedCube_OPT3001 opt3001;
 
 void setup() {
-    pinMode(2, INPUT_PULLUP);
-    pinMode(2, INPUT_PULLUP);
+    pinMode(SDA, PULLUP);
+    pinMode(SCL, PULLUP);
+
     Serial.begin(9600);
     lm92.ResultInCelsius = true;
-    // lm92.enableFaultQueue(true);
+    lm92.enableFaultQueue(true);
 
     initDisplay();
     display.clearDisplay();
@@ -30,12 +37,29 @@ void setup() {
     display.setCursor(0, 0);
     display.print("Hello!");
     display.display();
+
+    opt3001.begin(OPT3001_ADDRESS);
+    Serial.print("OPT3001 Manufacturer ID");
+    Serial.println(opt3001.readManufacturerID());
+    Serial.print("OPT3001 Device ID");
+    Serial.println(opt3001.readDeviceID());
+
+    configureSensor();
+    printResult("High-Limit", opt3001.readHighLimit());
+    printResult("Low-Limit", opt3001.readLowLimit());
+    Serial.println("----");
 }
 
 void loop() {
     delay(5000); // Take some time to open up the Serial Monitor
 
     Serial.println(lm92.readTemperature());
+    printResult("High-Limit", opt3001.readHighLimit());
+    printResult("Low-Limit", opt3001.readLowLimit());
+    OPT3001 result = opt3001.readResult();
+    printResult("OPT3001", result);
+    Serial.println("----");
+
     byte error, address;
     int nDevices;
 
@@ -136,4 +160,74 @@ void goToSleep(void) {
     Serial.println("Going to sleep now");
     Serial.flush();
     esp_deep_sleep_start();
+}
+
+void configureSensor() {
+    OPT3001_Config newConfig;
+
+    newConfig.RangeNumber = B1100;
+    newConfig.ConvertionTime = B0;
+    newConfig.Latch = B1;
+    newConfig.ModeOfConversionOperation = B11;
+
+    OPT3001_ErrorCode errorConfig = opt3001.writeConfig(newConfig);
+    if (errorConfig != NO_ERROR)
+        printError("OPT3001 configuration", errorConfig);
+    else {
+        OPT3001_Config sensorConfig = opt3001.readConfig();
+        Serial.println("OPT3001 Current Config:");
+        Serial.println("------------------------------");
+
+        Serial.print("Conversion ready (R):");
+        Serial.println(sensorConfig.ConversionReady, HEX);
+
+        Serial.print("Conversion time (R/W):");
+        Serial.println(sensorConfig.ConvertionTime, HEX);
+
+        Serial.print("Fault count field (R/W):");
+        Serial.println(sensorConfig.FaultCount, HEX);
+
+        Serial.print("Flag high field (R-only):");
+        Serial.println(sensorConfig.FlagHigh, HEX);
+
+        Serial.print("Flag low field (R-only):");
+        Serial.println(sensorConfig.FlagLow, HEX);
+
+        Serial.print("Latch field (R/W):");
+        Serial.println(sensorConfig.Latch, HEX);
+
+        Serial.print("Mask exponent field (R/W):");
+        Serial.println(sensorConfig.MaskExponent, HEX);
+
+        Serial.print("Mode of conversion operation (R/W):");
+        Serial.println(sensorConfig.ModeOfConversionOperation, HEX);
+
+        Serial.print("Polarity field (R/W):");
+        Serial.println(sensorConfig.Polarity, HEX);
+
+        Serial.print("Overflow flag (R-only):");
+        Serial.println(sensorConfig.OverflowFlag, HEX);
+
+        Serial.print("Range number (R/W):");
+        Serial.println(sensorConfig.RangeNumber, HEX);
+
+        Serial.println("------------------------------");
+    }
+}
+
+void printResult(String text, OPT3001 result) {
+    if (result.error == NO_ERROR) {
+        Serial.print(text);
+        Serial.print(": ");
+        Serial.print(result.lux);
+        Serial.println(" lux");
+    } else {
+        printError(text, result.error);
+    }
+}
+
+void printError(String text, OPT3001_ErrorCode error) {
+    Serial.print(text);
+    Serial.print(": [ERROR] Code #");
+    Serial.println(error);
 }
